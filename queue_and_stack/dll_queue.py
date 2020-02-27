74411a7545c8700f5e9d001ee605f8d1a5586082
yurika sakae <yurika.sun@gmail.com>
2020-02-26 23:38:54 -0800

    queue structure complete

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
 
@@ -7,13 +5,17 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # insersion and deletion is very fast for head and tail operation. queue is FIFO structure which works well with head tail pointer system of DLL
+
+        self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size = self.storage.length
 
     def dequeue(self):
-        pass
+        self.storage.remove_from_head()
+        self.size = self.storage.length
 
     def len(self):
-        pass
+        return self.size
