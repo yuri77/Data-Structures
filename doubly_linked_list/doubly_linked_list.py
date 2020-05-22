2a520dc797211e06d3212dad041ef4f421ca737b
yurika sakae <yurika.sun@gmail.com>
2020-05-21 23:08:55 -0700

    move to front/tail complete

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -103,13 +103,17 @@ class DoublyLinkedList:
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        head_value = node.value
+        self.delete(node)
+        self.add_to_head(head_value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        pass
+        tail_value = node.value
+        self.delete(node)
+        self.add_to_tail(tail_value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
