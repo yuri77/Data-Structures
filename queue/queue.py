e68c44bf798fbac0b7bae5f74ef185b9b3dcccdf
yurika sakae <yurika.sun@gmail.com>
2020-05-24 17:23:27 -0700

    queue SLL implementation complete

--- queue/queue.py
+++ queue/queue.py
@@ -17,38 +17,43 @@ linked list approach is faster with O(1). It can access the head right away
    Array pop method takes O(n) when the first element is taken out from the array, underlying index needs to be re-indexed and shifted. 
    Linked list approach is faster in Queue 
 """
+from singly_linked_list import LinkedList
+
+# class Queue:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return self.size
+
+#     def enqueue(self, value):
+#         self.size += 1
+#         self.storage.append(value)
+
+#     def dequeue(self):
+#         if self.size > 0:
+#             self.size -= 1
+#             return self.storage.pop(0)
+#         else:
+#             return None
 
 
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
         self.size += 1
-        self.storage.append(value)
+        self.storage.add_to_tail(value)
 
     def dequeue(self):
         if self.size > 0:
             self.size -= 1
-            return self.storage.pop(0)
+            return self.storage.remove_head()
         else:
             return None
-
-
-# class Queue:
-#     def __init__(self):
-#         self.size = 0
-#         # self.storage = ?
-
-#     def __len__(self):
-#         pass
-
-#     def enqueue(self, value):
-#         pass
-
-#     def dequeue(self):
-#         pass
